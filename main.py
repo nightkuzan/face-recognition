85b81fe8c817bb317ef617f5a9973e435e007a0c
nightkuzan <aekkarit12@gmail.com>
2023-02-09 14:05:59 +0700

    Gui

---
+++ main.py
@@ -0,0 +1,297 @@
+
+############################################# IMPORTING ################################################
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox as mess
+import tkinter.simpledialog as tsd
+import cv2,os
+import csv
+import numpy as np
+from PIL import Image
+import pandas as pd
+import datetime
+
+import time
+from PIL import ImageFont
+from PIL import ImageDraw
+import codecs
+
+
+######################################## FUNCTION ###############################################
+
+def clear():
+    txt.delete(0, 'end')
+    res = ""
+    message.configure(text= res)
+
+def clear2():
+    txt2.delete(0, 'end')
+    res = ""
+    message.configure(text= res)
+
+def TakeImages():
+    Id=(txt.get())
+    name=(txt2.get())
+    if((Id.isnumeric())):
+        cam = cv2.VideoCapture(0)
+        harcascadePath = "haarcascade_frontalface_default.xml"
+        detector=cv2.CascadeClassifier(harcascadePath)
+        sampleNum=0
+        while(True):
+            ret, img = cam.read()
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = detector.detectMultiScale(gray, 1.3, 5)
+            for (x,y,w,h) in faces:
+                cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+                #incrementing sample number
+                sampleNum=sampleNum+1
+                #saving the captured face in the dataset folder
+                cv2.imwrite("dataset/User."+Id +'.'+ str(sampleNum) + ".jpg", gray[y:y+h,x:x+w])
+                #display the frame
+                cv2.imshow('frame',img)
+            #wait for 100 miliseconds
+            if cv2.waitKey(100) & 0xFF == ord('q'):
+                break
+            # break if the sample number is morethan 100
+            elif sampleNum>60:
+                break
+        cam.release()
+        cv2.destroyAllWindows()
+        res = "Images Saved for ID : " + Id +" Name : "+ name
+        row = [Id , name]
+        with open('StudentDetails\StudentDetails.csv','a+') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerow(row)
+        csvFile.close()
+        message.configure(text= res)
+    else:
+        if((name.isalpha())):
+            res = "Enter Numeric Id"
+            message.configure(text= res)
+
+def TrainImages():
+    recognizer = cv2.face.LBPHFaceRecognizer_create()#recognizer = cv2.face.createLBPHFaceRecognizer()
+    harcascadePath = "haarcascade_frontalface_default.xml"
+    detector =cv2.CascadeClassifier(harcascadePath)
+    faces,Id = getImagesAndLabels("dataset")
+    recognizer.train(faces, np.array(Id))
+    recognizer.save("TrainingImageLabel\Trainner.yml")
+    res = "Image Trained"#+",".join(str(f) for f in Id)
+    message.configure(text= res)
+
+def getImagesAndLabels(path):
+    imagePaths=[os.path.join(path,f) for f in os.listdir(path)]
+    faces=[]
+    Ids=[]
+    for imagePath in imagePaths:
+        pilImage=Image.open(imagePath).convert('L')
+        imageNp=np.array(pilImage,'uint8')
+        Id=int(os.path.split(imagePath)[-1].split(".")[1])
+        faces.append(imageNp)
+        Ids.append(Id)
+    return faces,Ids
+
+def psw():
+    global key
+    key = tsd.askstring("Password","Enter Password",show="*")
+    if(key == "1234"):
+        res = "Login Success"
+        message.configure(text= res)
+    else:
+        res = "Login Failed"
+        message.configure(text= res)
+
+def TrackImages():
+    recognizer = cv2.face.LBPHFaceRecognizer_create()#cv2.createLBPHFaceRecognizer()
+    recognizer.read("TrainingImageLabel\Trainner.yml")
+    harcascadePath = "haarcascade_frontalface_default.xml"
+    faceCascade = cv2.CascadeClassifier(harcascadePath);
+    df=pd.read_csv("StudentDetails\StudentDetails.csv")
+    cam = cv2.VideoCapture(0)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    col_names =  ['Id','Name','Date','Time']
+    attendance = pd.DataFrame(columns = col_names)
+    while True:
+        ret, im =cam.read()
+        gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+        faces=faceCascade.detectMultiScale(gray, 1.2,5)
+        for(x,y,w,h) in faces:
+            cv2.rectangle(im,(x,y),(x+w,y+h),(225,0,0),2)
+            Id, conf = recognizer.predict(gray[y:y+h,x:x+w])
+            if(conf < 50):
+                ts = time.time()
+                date = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y')
+                timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+                aa=df.loc[df['Id'] == Id]['Name'].values
+                tt=str(Id)+"-"+aa
+                attendance.loc[len(attendance)] = [Id,aa,date,timeStamp]
+            else:
+                Id='Unknown'
+                tt=str(Id)
+            if(conf > 75):
+                noOfFile=len(os.listdir("ImagesUnknown"))+1
+                cv2.imwrite("ImagesUnknown\Image"+str(noOfFile) + ".jpg", im[y:y+h,x:x+w])
+            cv2.putText(im,str(tt),(x,y+h), font, 1,(255,255,255),2)
+        attendance=attendance.drop_duplicates(subset=['Id'],keep='first')
+        cv2.imshow('im',im)
+        if (cv2.waitKey(1)==ord('q')):
+            break
+    ts = time.time()
+    date = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y')
+    timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+    Hour,Minute,Second=timeStamp.split(":")
+    fileName="Attendance\Attendance_"+date+"_"+Hour+"-"+Minute+"-"+Second+".csv"
+    attendance.to_csv(fileName,index=False)
+    cam.release()
+######################################## USED STUFFS ############################################
+
+global key
+key = ''
+
+ts = time.time()
+date = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y')
+day,month,year=date.split("-")
+
+mont={'01':'มกราคม',
+      '02':'กุมภาพันธ์',
+      '03':'มีนาคม',
+      '04':'เมษายน',
+      '05':'พฤษภาคม',
+      '06':'มิถุนายน',
+      '07':'กรกฎาคม',
+      '08':'สิงหาคม',
+      '09':'กันยายน',
+      '10':'ตุลาคม',
+      '11':'พฤศจิกายน',
+      '12':'ธันวาคม'
+      }
+##################################################################################
+
+def tick():
+    time_string = time.strftime('%H:%M:%S')
+    clock.config(text=time_string)
+    clock.after(200,tick)
+######################################## GUI FRONT-END ###########################################
+
+window = tk.Tk()
+window.geometry("1280x720")
+window.resizable(True,False)
+window.title("โปรแกรมเช็คชื่อ ด้วยใบหน้า")
+window.configure(background='#262523')
+
+frame1 = tk.Frame(window, bg="#00aeff")
+frame1.place(relx=0.11, rely=0.17, relwidth=0.39, relheight=0.80)
+
+frame2 = tk.Frame(window, bg="#00aeff")
+frame2.place(relx=0.51, rely=0.17, relwidth=0.38, relheight=0.80)
+
+message3 = tk.Label(window, text="โปรแกรมเช็คชื่อ ด้วยใบหน้า" ,fg="white",bg="#262523" ,width=55 ,height=1,font=('times', 29, ' bold '))
+message3.place(x=10, y=10)
+
+frame3 = tk.Frame(window, bg="#c4c6ce")
+frame3.place(relx=0.52, rely=0.09, relwidth=0.09, relheight=0.07)
+
+frame4 = tk.Frame(window, bg="#c4c6ce")
+frame4.place(relx=0.36, rely=0.09, relwidth=0.16, relheight=0.07)
+
+datef = tk.Label(frame4, text = " "+day+" "+mont[month]+" "+year+"  |  ", fg="orange",bg="#262523" ,width=55 ,height=1,font=('times', 20, ' bold '))
+datef.pack(fill='both',expand=1)
+
+clock = tk.Label(frame3,fg="orange",bg="#262523" ,width=55 ,height=1,font=('times', 22, ' bold '))
+clock.pack(fill='both',expand=1)
+tick()
+
+head2 = tk.Label(frame2, text="                        ลงทะเบียนใบหน้า                             ", fg="black",bg="#3ece48" ,font=('times', 17, ' bold ') )
+head2.grid(row=0,column=0)
+
+head1 = tk.Label(frame1, text="                              แสดงรายการ                               ", fg="black",bg="#3ece48" ,font=('times', 17, ' bold ') )
+head1.place(x=0,y=0)
+
+lbl = tk.Label(frame2, text="รหัส",width=20  ,height=1  ,fg="black"  ,bg="#00aeff" ,font=('times', 17, ' bold ') )
+lbl.place(x=80, y=55)
+
+txt = tk.Entry(frame2,width=32 ,fg="black",font=('./angsau_0', 15, ' bold '))
+txt.place(x=30, y=88)
+
+lbl2 = tk.Label(frame2, text="ชื่อ นามสกุล",width=20  ,fg="black"  ,bg="#00aeff" ,font=('times', 17, ' bold '))
+lbl2.place(x=80, y=140)
+
+txt2 = tk.Entry(frame2, width=32 ,fg="black",font=('Courier', 15, ' bold ')  )
+txt2.place(x=30, y=173)
+
+message1 = tk.Label(frame2, text="1)ถ่ายรูป  >>>  2)บันทึกข้อมูล" ,bg="#00aeff" ,fg="black"  ,width=39 ,height=1, activebackground = "yellow" ,font=('times', 15, ' bold '))
+message1.place(x=7, y=230)
+
+message = tk.Label(frame2, text="" ,bg="#00aeff" ,fg="black"  ,width=39,height=1, activebackground = "yellow" ,font=('times', 16, ' bold '))
+message.place(x=7, y=450)
+
+lbl3 = tk.Label(frame1, text="ข้อมูล",width=20  ,fg="black"  ,bg="#00aeff"  ,height=1 ,font=('times', 17, ' bold '))
+lbl3.place(x=100, y=115)
+
+
+
+res=0
+exists = os.path.isfile("StudentDetails\StudentDetails.csv")
+if exists:
+    with open("StudentDetails\StudentDetails.csv", 'r') as csvFile1:
+        reader1 = pd.read_csv(csvFile1,encoding='utf-8')
+        for l in reader1:
+            res = res + 1
+    res = (res // 2)
+    csvFile1.close()
+else:
+    res = 0
+message.configure(text='จำนวนข้อมูลที่บันทึก  : '+str(res))
+
+##################### MENUBAR #################################
+
+menubar = tk.Menu(window,relief='ridge')
+filemenu = tk.Menu(menubar,tearoff=0)
+# filemenu.add_command(label='เปลี่ยนรหัส', command = change_pass)
+# filemenu.add_command(label='ติดต่อ', command = contact)
+filemenu.add_command(label='ออก',command = window.destroy)
+menubar.add_cascade(label='ตั้งค่า',font=('times', 29, ' bold '),menu=filemenu)
+
+################## TREEVIEW ATTENDANCE TABLE ####################
+
+tv= ttk.Treeview(frame1,height =13,columns = ('name','date','time','temp'))
+tv.column('#0',width=75)
+tv.column('name',width=110)
+tv.column('date',width=110)
+tv.column('time',width=110)
+tv.column('temp',width=71)
+tv.grid(row=2,column=0,padx=(0,0),pady=(150,0),columnspan=4)
+tv.heading('#0',text ='รหัส')
+tv.heading('name',text ='ชื่อ')
+tv.heading('date',text ='วันที่')
+tv.heading('time',text ='เวลา')
+tv.heading('temp',text ='อุณหภูมิ')
+
+
+###################### SCROLLBAR ################################
+
+scroll=ttk.Scrollbar(frame1,orient='vertical',command=tv.yview)
+scroll.grid(row=2,column=4,padx=(0,100),pady=(150,0),sticky='ns')
+tv.configure(yscrollcommand=scroll.set)
+
+###################### BUTTONS ##################################
+
+clearButton = tk.Button(frame2, text="ลบ", command=clear  ,fg="black"  ,bg="#ea2a2a"  ,width=11 ,activebackground = "white" ,font=('times', 11, ' bold '))
+clearButton.place(x=335, y=86)
+clearButton2 = tk.Button(frame2, text="ลบ", command=clear2  ,fg="black"  ,bg="#ea2a2a"  ,width=11 , activebackground = "white" ,font=('times', 11, ' bold '))
+clearButton2.place(x=335, y=172)    
+takeImg = tk.Button(frame2, text="ถ่ายรูป", command=TakeImages  ,fg="white"  ,bg="blue"  ,width=34  ,height=1, activebackground = "white" ,font=('times', 15, ' bold '))
+takeImg.place(x=30, y=300)
+trainImg = tk.Button(frame2, text="บันทึกข้อมูล", command=psw ,fg="white"  ,bg="blue"  ,width=34  ,height=1, activebackground = "white" ,font=('times', 15, ' bold '))
+trainImg.place(x=30, y=380)
+trackImg = tk.Button(frame1, text="เปิดกล้อง", command=TrackImages  ,fg="black"  ,bg="yellow"  ,width=35  ,height=1, activebackground = "white" ,font=('times', 15, ' bold '))
+trackImg.place(x=30,y=50)
+quitWindow = tk.Button(frame1, text="ออกจากโปรแกรม", command=window.destroy  ,fg="black"  ,bg="red"  ,width=35 ,height=1, activebackground = "white" ,font=('times', 15, ' bold '))
+quitWindow.place(x=30, y=450)
+
+##################### END ######################################
+
+window.configure(menu=menubar)
+window.mainloop()
+
+####################################################################################################
