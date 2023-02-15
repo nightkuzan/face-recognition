e9264ed23fae4f4872ed7475fea965415ec8b816
nightkuzan <aekkarit12@gmail.com>
2023-02-16 02:06:54 +0700

    change color background frame

--- main.py
+++ main.py
@@ -257,10 +257,10 @@ window.resizable(True,False)
 window.title("โปรแกรมเช็คชื่อ ด้วยใบหน้า")
 window.configure(background='#262523')
 
-frame1 = tk.Frame(window, bg="#00aeff")
+frame1 = tk.Frame(window, bg="#00ffca")
 frame1.place(relx=0.11, rely=0.17, relwidth=0.39, relheight=0.80)
 
-frame2 = tk.Frame(window, bg="#00aeff")
+frame2 = tk.Frame(window, bg="#00ffca")
 frame2.place(relx=0.51, rely=0.17, relwidth=0.38, relheight=0.80)
 
 message3 = tk.Label(window, text="โปรแกรมเช็คชื่อ ด้วยใบหน้า" ,fg="white",bg="#262523" ,width=55 ,height=1,font=('times', 29, ' bold '))
@@ -285,25 +285,25 @@ head2.grid(row=0,column=0)
 head1 = tk.Label(frame1, text="                              แสดงรายการ                               ", fg="black",bg="#3ece48" ,font=('times', 17, ' bold ') )
 head1.place(x=0,y=0)
 
-lbl = tk.Label(frame2, text="รหัส",width=20  ,height=1  ,fg="black"  ,bg="#00aeff" ,font=('times', 17, ' bold ') )
+lbl = tk.Label(frame2, text="รหัส",width=20  ,height=1  ,fg="black"  ,bg="#00ffca" ,font=('times', 17, ' bold ') )
 lbl.place(x=80, y=55)
 
 txt = tk.Entry(frame2,width=32 ,fg="black" ,bg="white",font=('times', 15, ' bold '))
 txt.place(x=30, y=88)
 
-lbl2 = tk.Label(frame2, text="ชื่อ นามสกุล",width=20  ,fg="black"  ,bg="#00aeff" ,font=('times', 17, ' bold '))
+lbl2 = tk.Label(frame2, text="ชื่อ นามสกุล",width=20  ,fg="black"  ,bg="#00ffca" ,font=('times', 17, ' bold '))
 lbl2.place(x=80, y=140)
 
 txt2 = tk.Entry(frame2, width=32 ,fg="black",bg="white",font=('Courier', 15, ' bold ')  )
 txt2.place(x=30, y=173)
 
-message1 = tk.Label(frame2, text="1)ถ่ายรูป  >>>  2)บันทึกข้อมูล" ,bg="#00aeff" ,fg="black"  ,width=39 ,height=1, activebackground = "yellow" ,font=('times', 15, ' bold '))
+message1 = tk.Label(frame2, text="1)ถ่ายรูป  >>>  2)บันทึกข้อมูล" ,bg="#00ffca" ,fg="black"  ,width=39 ,height=1, activebackground = "yellow" ,font=('times', 15, ' bold '))
 message1.place(x=7, y=230)
 
-message = tk.Label(frame2, text="" ,bg="#00aeff" ,fg="black"  ,width=39,height=1, activebackground = "yellow" ,font=('times', 16, ' bold '))
+message = tk.Label(frame2, text="" ,bg="#00ffca" ,fg="black"  ,width=39,height=1, activebackground = "yellow" ,font=('times', 16, ' bold '))
 message.place(x=7, y=450)
 
-lbl3 = tk.Label(frame1, text="ข้อมูล",width=20  ,fg="black"  ,bg="#00aeff"  ,height=1 ,font=('times', 17, ' bold '))
+lbl3 = tk.Label(frame1, text="ข้อมูล",width=20  ,fg="black"  ,bg="#00ffca"  ,height=1 ,font=('times', 17, ' bold '))
 lbl3.place(x=100, y=115)
 
 
